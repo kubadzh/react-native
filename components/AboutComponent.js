d5b4e69f7f65f40bb5aa5096c8b2208e63e52243
Kuba <dzcapital@gmail.com>
2021-02-11 15:45:57 -0500

    Gestures

--- components/AboutComponent.js
+++ components/AboutComponent.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { ScrollView, Text, FlatList } from 'react-native';
 import { Card, ListItem } from 'react-native-elements';
+import * as Animatable from 'react-native-animatable';
 import { connect } from 'react-redux';
 import { baseUrl } from '../shared/baseUrl';
 import Loading from './LoadingComponent';
@@ -56,27 +57,32 @@ class About extends Component {
       );
     }
 
-    if(this.props.partners.errMess) {
-        return(
-            <ScrollView>
+    if (this.props.partners.errMess) {
+      return (
+          <ScrollView>
+              <Animatable.View animation='fadeInDown' duration={2000} delay={1000}>
                   <Mission />
-                  <Card title="Community Partners">
-                        <Text>{this.props.partners.errMess}</Text>
+                  <Card
+                      title="Community Partners">
+                      <Text>{this.props.partners.errMess}</Text>
                   </Card>
-            </ScrollView>
-        );
-    }
-
-    return (
+              </Animatable.View>
+          </ScrollView>
+      );
+  }
+  return (
       <ScrollView>
-        <Mission />
-        <Card title="Community Partners">
-          <FlatList
-            data={this.props.partners.partners}
-            renderItem={renderPartner}
-            keyExtractor={(item) => item.id.toString()}
-          />
-        </Card>
+          <Animatable.View animation='fadeInDown' duration={2000} delay={1000}>
+              <Mission />
+              <Card
+                  title="Community Partners">
+                  <FlatList
+                      data={this.props.partners.partners}
+                      renderItem={renderPartner}
+                      keyExtractor={item=>item.id.toString()}
+                  />
+              </Card>
+          </Animatable.View>
       </ScrollView>
     );
   }
